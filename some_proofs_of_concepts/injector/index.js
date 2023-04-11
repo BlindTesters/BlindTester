040765b7f34fd6ca9770e7b895f81731d71404a3
kevinberret <berret.kevin@gmail.com>
2023-04-11 21:40:11 +0200

    trace moment js format

---
+++ some_proofs_of_concepts/injector/index.js
@@ -0,0 +1,13 @@
+const Injector = require('./injector.js');
+
+const moment = require('moment')
+const momentInst = moment();
+const injector = new Injector(momentInst, 'format', __filename, 'SSE23-derivative');
+
+console.log(momentInst.format('MMMM Do YYYY, h:mm:ss a'));
+console.log(momentInst.format('dddd'));
+console.log(momentInst.format("MMM Do YY"));
+console.log(momentInst.format('YYYY [escaped] YYYY'));
+console.log(momentInst.format());
+
+injector.writeTrace()
