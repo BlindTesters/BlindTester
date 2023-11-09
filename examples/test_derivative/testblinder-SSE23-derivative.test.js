4a103fd279f07d2af3342cdcd59ca9ea50aa8516
Lièvre Loïc <llievre@protonmail.com>
2023-11-09 08:58:34 +0100

    Clean some unused files in examples

--- examples/test_derivative/testblinder-SSE23-derivative.test.js
+++
@@ -1,7 +0,0 @@
-const mathjs = require('mathjs');
-
-describe('blindtester-SSE23-derivative', () => {
-    test('mathjs.derivative("x^2","x") should returns {"mathjs":"OperatorNode","op":"*","fn":"multiply","args":[{"mathjs":"ConstantNode","value":2.0},{"mathjs":"SymbolNode","name":"x"}],"implicit":false,"isPercentage":false}', () => {
-        expect(JSON.parse(JSON.stringify(mathjs.derivative("x^2","x")))).toMatchObject({"mathjs":"OperatorNode","op":"*","fn":"multiply","args":[{"mathjs":"ConstantNode","value":2.0},{"mathjs":"SymbolNode","name":"x"}],"implicit":false,"isPercentage":false});
-    });
-});
